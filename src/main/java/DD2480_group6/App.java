deb7d251c8339ddbbb56f702747de6dad0032cd8
iZafiro <60047972+iZafiro@users.noreply.github.com>
2020-02-06 14:04:16 +0100

    Feat: Implemented a simple math program and its unit tests. Closes issue #3 (#5)
    * Feat: Implemented a simple math program and its unit tests. Issue #3
    
    * Removed IDE directory
    
    * #fix fixed the second assert.
    
    It now produces the expected behavior.
    
    Co-authored-by: fwald <38955346+fwald@users.noreply.github.com>
    closes #3

--- src/main/java/DD2480_group6/App.java
+++ src/main/java/DD2480_group6/App.java
@@ -1,13 +1,22 @@
 package DD2480_group6;
 
 /**
- * Hello world!
- *
+ * SIMPLE MATH PROGRAM
+ * TO-DO: DOCUMENTATION
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public int addTwoPlusTwo() {
+        return 2+2;
+    }
+
+    public int multiply(int x1, int x2) {
+        return x1*x2;
+    }
+
+    public int fibonacci(int n) {
+        if(n <= 1)
+            return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
--- src/test/java/DD2480_group6/AppTest.java
+++ src/test/java/DD2480_group6/AppTest.java
@@ -5,7 +5,8 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * UNIT TESTS FOR SIMPLE MATH PROGRAM
+ * TO-DO DOCUMENTATION
  */
 public class AppTest 
     extends TestCase
@@ -31,8 +32,22 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public static void addTwoPlusTwo()
     {
-        assertTrue( true );
+        App app = new App();
+        assertTrue(app.addTwoPlusTwo() == 4);
+        assertFalse(app.addTwoPlusTwo() == 5);
+    }
+    public static void testMultiply()
+    {
+        App app = new App();
+        assertTrue(app.multiply(5, 5) == 25);
+        assertFalse(app.multiply(1, 0) == 1);
+    }
+    public static void fibonacci()
+    {
+        App app = new App();
+        assertTrue(app.fibonacci(3) == 2);
+        assertFalse(app.fibonacci(1) == 0);
     }
 }
